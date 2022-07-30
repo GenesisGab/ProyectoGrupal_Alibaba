083987097d7d4ffacf1239b9a68ad95f60da6e78
Gaby <ggasubuyuj@scl.edu.gt>
2022-07-30 14:54:52 -0600

    Se crearon los archivos principales y se agrego el Login

---
+++ js/main.js
@@ -0,0 +1,18 @@
+/*
+import {revisarusuario} from "./login.js"
+
+function recuperar_datos(e) {
+    e.preventDefault(); //<-- Evita que el formulario se recargue
+
+    var username = document.querySelector("#username").value;
+    var password = document.querySelector("#password").value;
+
+    //Programa login
+    revisarusuario(username,password);
+}
+
+// Acción del boton
+var bt_entrar = document.querySelector("#login");//<-- Instancia del formulario
+bt_entrar.addEventListener('submit',recuperar_datos); //<-- Evento
+
+*/
